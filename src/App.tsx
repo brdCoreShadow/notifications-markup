80b41b4b52d5a945a8b41960ec050ab8cfd2e705
Oleksandr-Berd <alex.berd86@gmail.com>
2025-07-13 11:40:08 +0300

    header and event notes are made

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,62 @@
-import * as SC from "./AppStyled"
+import * as SC from "./AppStyled";
 
-function App() {
+import webereAva from "./assets/images/avatar-mark-webber.webp";
+import grayAva from "./assets/images/avatar-angela-gray.webp"
+import thompsonAva from "./assets/images/avatar-jacob-thompson.webp"
 
+const App: React.FC = () => {
   return (
     <SC.AppCon>
-     
+      <SC.Header>
+        <SC.LogoCon>
+          <h1>Notifications</h1>
+          <SC.LogoBox>
+            <span>3</span>
+          </SC.LogoBox>
+        </SC.LogoCon>
+        <SC.HeaderBtn type="button">Mark all as read</SC.HeaderBtn>
+      </SC.Header>
+      <SC.EventsList>
+        <SC.EventsItem>
+          <SC.AvaThumb>
+            <img src={webereAva} alt="webere_ava" />
+          </SC.AvaThumb>
+          <div>
+            <SC.Content>
+              <SC.Login>Mark Webber</SC.Login> reacted to your recent post{" "}
+              <SC.Event>My first tournament today!</SC.Event>
+            </SC.Content>
+            <p>1m ago</p>
+            <div></div>
+          </div>
+        </SC.EventsItem>
+        <SC.EventsItem>
+          <SC.AvaThumb>
+            <img src={grayAva} alt="gray_ava" />
+          </SC.AvaThumb>
+          <div>
+            <SC.Content>
+              <SC.Login>Angela Gray</SC.Login> folowed you
+            </SC.Content>
+            <p>5m ago</p>
+            <div></div>
+          </div>
+        </SC.EventsItem>
+        <SC.EventsItem>
+           <SC.AvaThumb>
+            <img src={thompsonAva} alt="thompson_ava" />
+          </SC.AvaThumb>
+          <div>
+            <SC.Content>
+              <SC.Login>Jacob Thompson</SC.Login> has joined your group <SC.Event>Chess Club</SC.Event>
+            </SC.Content>
+            <p>1 day ago</p>
+            <div></div>
+          </div>
+        </SC.EventsItem>
+      </SC.EventsList>
     </SC.AppCon>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/AppStyled.ts
+++ src/AppStyled.ts
@@ -1,3 +1,118 @@
 import styled from "@emotion/styled";
 
-export const AppCon = styled.div``
+export const AppCon = styled.div`
+  padding-top: 24px;
+  padding-bottom: 28px;
+  padding-left: 16px;
+  padding-right: 16px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 24px;
+`;
+
+export const LogoCon = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  & > h1 {
+    margin-right: 8px;
+
+    font-size: 20px;
+    font-weight: 800;
+  }
+`;
+
+export const LogoBox = styled.div`
+  padding-top: 2px;
+  padding-bottom: 4px;
+  padding-left: 12px;
+  padding-right: 12px;
+
+  background-color: #0a327b;
+
+  border-radius: 8px;
+
+  & > span {
+    font-size: 16px;
+    font-weight: 800;
+
+    color: #fff;
+  }
+`;
+
+export const HeaderBtn = styled.button`
+  font-size: 14px;
+  font-weight: 500;
+  font-family: "Jakarta";
+
+  color: #5e6778;
+
+  background-color: transparent;
+`;
+
+export const EventsList = styled.ul`
+  margin-bottom: 12px;
+`;
+
+export const EventsItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+
+  padding-top: 16px;
+  padding-bottom: 16px;
+  padding-left: 16px;
+  padding-right: 28px;
+
+  background-color: #f7fafd;
+
+  border-radius: 8px;
+
+  &:not(:last-of-type) {
+    margin-bottom: 12px;
+  }
+`;
+
+export const AvaThumb = styled.div`
+width: 39px;
+height: 39px;
+
+margin-right: 12px;
+
+& > img{
+   width: 100%;
+   height: 100%;
+
+   object-fit: cover;
+}
+`
+
+export const Content = styled.p`
+margin-bottom: 4px;
+
+font-size: 14px;
+font-weight: 500;
+
+color:#5E6778;
+`
+
+export const Login = styled.span`
+font-size: 14px;
+font-weight: 800;
+
+color: #1C202B;
+`
+
+export const Event = styled.span`
+font-size: 14px;
+font-weight: 700;
+
+color:#5E6778;
+`
